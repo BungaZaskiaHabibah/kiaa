4f84d352111baff46d48ca58012663214ba448cb
Bungaaa <bzaskia78@gmail.com>
2024-09-23 19:05:25 +0700

    first commit

---
+++ src/Component/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero-first'>
+      <h1>Kopi Kenangan stands for Coffee Memories.</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <img src="https://images.tokopedia.net/img/KRMmCm/2022/8/26/ac829485-a518-4ff4-977c-9e0059555501.jpg"
+       alt="" className='kopi' />
+       <h3>promo of the month</h3>
+       <img src="https://images.squarespace-cdn.com/content/v1/5fa1095912d2fc6dfc63ac9c/187c3acb-dcb1-4ece-96b3-48a1b02267e0/Pesta+Cashback+-+IGF+-+AGUSTUS.jpg?format=750w" 
+       alt="" className='promo' />
+       <img src="https://images.squarespace-cdn.com/content/v1/5fa1095912d2fc6dfc63ac9c/e22a38f7-240e-4f5d-a9c6-403f821c7a40/Gebyar+Kenangan+IGS+IGF-03.jpg?format=750w"
+        alt="" className='diskon' />
+        <br />
+        <br />
+        <br />
+        <div className="footer">
+        <h2>Beli Disini</h2>
+        <br />
+        <div className="itu">
+        <a href="#">Download Aplikasi Kopi Kenangan</a>
+        </div>
+        <div className="tlet">
+          <a href="#">Outlet Terdekat Daerah Sini</a>
+        </div>
+        <p>Consumer Complaints Service Contact Information
+Directorate General of Consumer Protection and Trade Compliance, Ministry of Trade of the Republic of Indonesia
+Whatsapp Diktjen PKTN: 0853-1111-1010</p>
+       <h4>Hak Cipta ©2020 PT Bumi Berkah Boga</h4>
+       
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+import './Navbar.css'
+//import './assets./Navbar./logo.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-awal'>
+        <div className="info">
+            <ul><a href="#">Home</a></ul>
+            <ul><a href="#">About</a></ul>
+            <ul><a href="#">Order</a></ul>
+        </div>
+      <img src="https://images.squarespace-cdn.com/content/v1/5fa1095912d2fc6dfc63ac9c/dd9b911d-7295-43fc-9452-17a585607187/logo.png?format=1500w" alt="" className='logo' />
+    </div>
+      
+      
+  )
+}
+
+export default Navbar
